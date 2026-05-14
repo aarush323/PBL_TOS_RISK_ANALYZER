4f9379e94cb5d7984bb4a74eb810edd008312f9f
prathameshk421 <prathameshkinagi421@gmail.com>
2026-05-14 21:18:02 +0530

    refactor: remove Anthropics font import and update CSS links in index.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'anthropic-fonts'
 import './index.css'
 import App from './App.jsx'
 import AppProviders from '@/app/providers/AppProviders.jsx'
